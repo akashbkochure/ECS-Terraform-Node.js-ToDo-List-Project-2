378cbd33d59627cf275922e4cb5dc2222cfed9a2
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-07-06 12:55:32 +0530

    Update lb.tf

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -17,8 +17,8 @@ resource "aws_lb_target_group" "group" {
 
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = "8000"
-  protocol          = "TCP"
+  port              = "80"
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
